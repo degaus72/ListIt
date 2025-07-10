1fbb701fad526d0b6ff4763f58e4a6afc9986c8f
Matteo <gosimatteo@gmail.com>
2025-07-10 09:54:56 +0100

    First Commit

---
+++ ListItTests/ListItTests.swift
@@ -0,0 +1,17 @@
+//
+//  ListItTests.swift
+//  ListItTests
+//
+//  Created by Matteo Gosi on 10/07/2025.
+//
+
+import Testing
+@testable import ListIt
+
+struct ListItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
